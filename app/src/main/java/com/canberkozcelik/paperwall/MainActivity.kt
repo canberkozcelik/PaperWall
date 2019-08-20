87d8612fade0248851194678c804ab42075e7de1
Canberk Ozcelik <canberk.ozcelik@loodos.com>
2019-08-20 13:45:19 +0300

    removed commercial images and ad ids

--- app/src/main/java/com/canberkozcelik/paperwall/MainActivity.kt
+++ app/src/main/java/com/canberkozcelik/paperwall/MainActivity.kt
@@ -2,12 +2,44 @@ package com.canberkozcelik.paperwall
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mInterstitialAd: InterstitialAd
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this) {}
+
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "<ENTER YOUR OWN>"
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+        mInterstitialAd.adListener = object : AdListener() {
+
+            override fun onAdClosed() {
+                super.onAdClosed()
+                finish()
+            }
+
+            override fun onAdFailedToLoad(p0: Int) {
+                super.onAdFailedToLoad(p0)
+                Timber.d("%d", p0)
+            }
+        }
+    }
+
+    fun showAd() {
+        mInterstitialAd.isLoaded.let { mInterstitialAd.show() }
     }
 }
--- app/src/main/java/com/canberkozcelik/paperwall/successpage/SuccessPageFragment.kt
+++ app/src/main/java/com/canberkozcelik/paperwall/successpage/SuccessPageFragment.kt
@@ -6,29 +6,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.canberkozcelik.paperwall.MainActivity
 import com.canberkozcelik.paperwall.R
 import com.canberkozcelik.paperwall.databinding.FragmentSuccessPageBinding
-import java.util.*
-import kotlin.concurrent.schedule
 
 /**
  * Created by canberkozcelik on 2019-05-26.
  */
 class SuccessPageFragment : Fragment() {
 
-    private lateinit var binding : FragmentSuccessPageBinding
+    private lateinit var binding: FragmentSuccessPageBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = DataBindingUtil.inflate(inflater,
-            R.layout.fragment_success_page, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_success_page, container, false
+        )
         return binding.root
     }
 
     override fun onResume() {
         super.onResume()
-        Timer().schedule(2000) {
-            fragmentManager?.popBackStack()
-            activity?.finish()
-        }
+        (activity as MainActivity).showAd()
     }
+
 }
